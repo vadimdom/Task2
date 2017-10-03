4faaaa6639532bf1bc08cca98ce38252719cd22d
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-03 16:20:49 +0300

    Task8

---
+++ 1-basic/js/Task8.js
@@ -0,0 +1,7 @@
+var rand = Math.round(Math.random()*10);
+var choice = prompt("Make your choice:");
+if (choice == rand){
+    alert("Good Work");
+}else{
+    alert("Not matched! Right answer is: " + rand);
+}
