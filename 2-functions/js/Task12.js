37e8b6d1373e24b8db4a381f324bf241ba551649
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-04 14:45:31 +0300

    Task12

---
+++ 2-functions/js/Task12.js
@@ -0,0 +1,18 @@
+function isPerfect(num1){
+    var num = +num1;
+    var sum = 0;
+    for(var i = 1; i <= Math.ceil(num/2); i++){
+            if (num%i == 0){
+                sum += i;
+            }else{
+                continue;
+            }
+    }
+    if (sum == num){
+        alert(num + " is a perfect number!");
+    }else{
+        alert(num + " is not a perfect number!");
+    }
+}
+
+isPerfect(prompt("Введите число для проверки: "));
