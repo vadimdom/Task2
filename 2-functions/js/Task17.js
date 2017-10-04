6809df99416ce91febff6c1cc19613f56b6e55db
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-04 17:26:54 +0300

    Task17

---
+++ 2-functions/js/Task17.js
@@ -0,0 +1,22 @@
+function theCount(str){
+    var uni = "";
+    for (var i = 0; i <= str.length-1; i++){
+        if (uni.indexOf(str.charAt(i)) < 0){
+            uni += str[i];
+        }
+    }
+    alert("Все уникальные символы: " + uni);
+    for (var i = 0; i<= uni.length-1; i++){
+        var col = 0;
+        var pos = 0;
+        while (true) {
+          var foundPos = str.indexOf(uni[i], pos);
+          if (foundPos == -1) break;
+          col++;
+          pos = foundPos + 1; 
+        }
+        alert("Количество вхождений символа '" + uni[i] + "' равно: " +col);
+    }   
+}
+
+theCount(prompt("Введите строку: "));
