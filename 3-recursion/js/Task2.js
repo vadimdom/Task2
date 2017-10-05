f569f6ed6fdfe16f2b77365408483b56b8d9b83c
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-05 03:25:36 +0300

    Task2+

--- 3-recursion/js/Task2.js
+++ 3-recursion/js/Task2.js
@@ -1,13 +1,18 @@
 function delitel(a, b){
-    if ((a >= b && a % b === 0) || a === b){
-        return b;
-    }else if ( b >= a && b % a === 0){
-        return a
-    }else if (a % 2 === 0 && b % 2 === 0){
-        return 2*delitel(a/2, b/2)
-    }else{
-        return 1;
+    if (a === b){
+        return a;
     }
+    if (a > b){
+        var big = a;
+        var low = b;
+    }else{
+        var big = b;
+        var low = a;
+    }
+    if (big % low === 0){
+        return low;
+    }
+    return delitel(low, big % low);
 }
 
 alert("Наибольший общий делитель: " + delitel(+(prompt("Введите первое число: ")), +(prompt("Введите второе число: "))));
