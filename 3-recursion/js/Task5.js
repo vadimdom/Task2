fc8eaa9615b4322f9880d9ec4f6d617d8be9447e
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-05 04:23:52 +0300

    Task1

--- 3-recursion/js/Task5.js
+++
@@ -1,9 +0,0 @@
-function exp(a, b){
-    if (b === 1){
-        return a;
-    }else{
-        return a*exp(a, b-1);
-    }
-}
-
-alert("Целые числа из промежутка: " + exp(+(prompt("Введите основание: ")), +(prompt("Введите степень: "))));
