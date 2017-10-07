21063c43c289ecda5efc2b3170317273f97410c7
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-07 20:10:58 +0300

    Task4

---
+++ 5-array/js/Task4.js
@@ -0,0 +1,20 @@
+function last(arr, n){
+    if (arr[0] == undefined ){
+        return arr;
+    }else if (n >= arr.length){
+        return arr;
+    }else if (n >=1){
+        return arr.slice(arr.length-n);
+    }else if (n == undefined){
+        return arr[arr.length-1];
+    }else if (n < 0){
+        var arr1 = [];
+        return arr1;
+    }
+}
+
+console.log(last([7, 9, 0, -2])); 
+console.log(last([],3));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
+console.log(last([7, 9, 0, -2],-3));
