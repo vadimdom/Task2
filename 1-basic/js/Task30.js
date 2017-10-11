6c499e33df7306bfc29d1d91e97a032ff1557617
Vadim Chernikov <vadim.dom_96@mail.ru>
2017-10-11 19:25:11 +0300

    deleted basic

--- 1-basic/js/Task30.js
+++
@@ -1,6 +0,0 @@
-var str = prompt("Введите строку: ");
-if (str.indexOf("Script") === 4){
-    alert(str.substring(0, 4) + str.substring(4 + "Script".length));
-}else{
-    alert(str);
-}
--- 1-basic/js/Task8.js
+++
@@ -1,7 +0,0 @@
-var rand = Math.round(Math.random()*10);
-var choice = +(prompt("Make your choice:"));
-if (choice === rand){
-    alert("Good Work");
-}else{
-    alert("Not matched! Right answer is: " + rand);
-}
